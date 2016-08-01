66bcc00b5412e00d939fac6ae4f62168a29a8c04
Mahati Gollamudi <github email address>
2016-07-31 22:37:24 -0400

    Inheritance Lab Complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+
+class Student < User
+
+  attr_reader :knowledge
+
+## @@knowledge = []
+
+  def initialize
+    @knowledge = []
+  end
+
+  # why can't @knowledge be a class variable?
+  # tried it and it failed the test
+
+  def learn(know_string)
+    @knowledge << know_string
+  end
 
 end
